80e878c0031bd3486b290cc90d5e0e7cec6268ba
josemartinrocha <jmmr@holacons.com>
2021-04-28 10:09:46 +0200

    fix apiController calls

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,6 +1,5 @@
 const https = require('https');
-var baseUrl = 'https://cardano-mainnet.blockfrost.io/api/v0/';
-var host = 'https://cardano-mainnet.blockfrost.io';
+var host = 'cardano-mainnet.blockfrost.io';
 var basePath = '/api/v0/';
 var blockFrostApiKey = 'XXX';
 
@@ -37,22 +36,31 @@ exports.getAddrUtxos = function(req, res) {
 
 exports.getTxUtxos = function(req, res) {
     var txHash = req.params.txHash;
-    var url = baseUrl + `txs/${txHash}/utxos`;
-    console.log('Url: ' + url);
-	var request = http.request({'url': url,
-                            'project_id': blockFrostApiKey
-                           }, 
-                            function (response) {
-                             console.log('STATUS: ' + response.statusCode);
-                             if (response.statusCode !== 200) {
-                                res.send(500, 'err');
-                             }
-                             console.log('HEADERS: ' + JSON.stringify(response.headers));
-                             response.setEncoding('utf8');
-                             response.on('data', function (chunk) {
-                               console.log('BODY: ' + chunk);
-                               res.status(200).jsonp(chunk);
-                             });
-                           });
-    request.end();
+
+    var options = {
+        host : host,
+        path: basePath + `addresses/${address}/utxos`,
+        headers: {
+            "project_id": blockFrostApiKey
+        },
+    };
+
+	https.get(options, (resp) => {
+        let data = '';
+    
+        // A chunk of data has been received.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+    
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            console.log(JSON.parse(data).explanation);
+            res.status(200).jsonp(data);
+        });
+    
+        }).on("error", (err) => {
+            console.log("Error: " + err.message);
+            res.send(500, 'err');
+        });
 };
