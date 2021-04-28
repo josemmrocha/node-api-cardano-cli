80df78e5a04535fe849883b63ead40f538072d08
josemartinrocha <jmmr@holacons.com>
2021-04-28 12:43:32 +0200

    revert

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -9,12 +9,19 @@ exports.getAddrUtxos = function(req, res) {
 
     var options = tools.getBlockfrostOptions(host, basePath + `addresses/${address}/utxos`, blockFrostApiKey);
 
-    try {
-        let res = await tools.httpGet(options);
-        res.status(200).jsonp(data);
-      } catch (err) {
+    https.get(options, (resp) => {
+        let data = '';
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });  
+        resp.on('end', () => {
+            console.log(JSON.parse(data).explanation);
+            res.status(200).jsonp(data);
+        });
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
         res.send(500, 'err');
-    }
+    });
 };
 
 exports.getTxUtxos = function(req, res) {
@@ -24,20 +31,15 @@ exports.getTxUtxos = function(req, res) {
 
 	https.get(options, (resp) => {
         let data = '';
-    
-        // A chunk of data has been received.
         resp.on('data', (chunk) => {
             data += chunk;
-        });
-    
-        // The whole response has been received. Print out the result.
+        });  
         resp.on('end', () => {
             console.log(JSON.parse(data).explanation);
             res.status(200).jsonp(data);
         });
-    
-        }).on("error", (err) => {
-            console.log("Error: " + err.message);
-            res.send(500, 'err');
-        });
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+        res.send(500, 'err');
+    });
 };
--- controllers/tools/tools.js
+++ controllers/tools/tools.js
@@ -9,22 +9,5 @@ module.exports = {
                 "project_id": blockFrostApiKey
             },
         };
-    },
-    httpGet: function (options) {
-        return new Promise ((resolve, reject) => {
-            https.get(options, (resp) => {
-                let data = '';
-                        resp.on('data', (chunk) => {
-                    data += chunk;
-                });
-            
-                resp.on('end', () => {
-                    resolve(data);
-                });
-            }).on("error", (err) => {
-                reject(err);
-            });
-        });
-        
     }
 };
