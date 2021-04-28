7387f70b35b5f9fca48ebb893945d59adeaf4247
josemartinrocha <jmmr@holacons.com>
2021-04-28 14:50:19 +0200

    fix errors

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -2,29 +2,24 @@ const axios = require('axios');
 
 exports.scanAndSend = function(req, res) {
     var address = req.params.addr;
-    res.send(200, 'running');
+    res.status(200).send('running');
 
     getUtxos(address).then(
         (response) => {
             if (response) {
-                var cont = 0;
                 response.forEach(element => {
                     console.log('Getting inputs for hash: ' + element.utxo);
                     getOuputsFromUtxo(element.utxo).then(
                         (result) => {
                             console.log('result: ' + result);
-                            if (cont === response.length-1) {
-                                finish();
-                            }
-                            cont++;
                         },
                         (error) => {
-                            res.send(500, 'err');
+                            res.status(500).send('err');
                         }
                     );
                 });
             } else {
-                res.send(500, 'err');
+                res.status(500).send('err');
             }
         }, 
         (error) => {
