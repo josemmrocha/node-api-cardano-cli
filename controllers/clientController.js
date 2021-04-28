245cb40cdc6190e7c5e0c9e5c632d5a95c1aefba
josemartinrocha <jmmr@holacons.com>
2021-04-28 14:36:15 +0200

    refactor clientController

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -2,32 +2,56 @@ const axios = require('axios');
 
 exports.scanAndSend = function(req, res) {
     var address = req.params.addr;
-    getUtxos(address).then((response) => {
-        console.log(response);
-        if (response) {
-            response.forEach(element => {
-                console.log('Element TX HASH: ' + element.utxo);
-            });
-            res.send(200, response[0].utxo);
-        } else {
-            res.send(500, 'err');
-        }
-      }, (error) => {
-        console.log(error);
-        res.status(500).send('err');
-      });
+    getUtxos(address).then(
+        (response) => {
+            if (response) {
+                var cont = 0;
+                response.forEach(element => {
+                    console.log('Getting inputs for hash: ' + element.utxo);
+                    getOuputsFromUtxo(element.utxo).then(
+                        (result) => {
+                            console.log('result: ' + result);
+                            if (cont === response.length-1) {
+                                finish();
+                            }
+                            cont++;
+                        },
+                        (error) => {
+                            res.send(500, 'err');
+                        }
+                    );
+                });
+            } else {
+                res.send(500, 'err');
+            }
+        }, 
+        (error) => {
+            res.status(500).send('err');
+    });
 };
 
 async function getUtxos(addr) {
-    var url = 'http://localhost:4200/api/' + '/utxos/' + addr;
+    var url = 'http://localhost:4200/api/' + 'utxos/' + addr;
 
     try {
         let res = await axios.get(url);
-        let data = res.data;
-        console.log(data);
-        return data;
+        return res.data;
     } catch (error) {
-        console.log(error);
         return undefined;
     }   
-  }
+}
+
+async function getOuputsFromUtxo(txHash) {
+    var url = 'http://localhost:4200/api/' + 'txUtxos/' + txHash;
+
+    try {
+        let res = await axios.get(url);
+        return res.data;
+    } catch (error) {
+        return undefined;
+    }   
+}
+
+function finish() {
+    res.send(200, true);
+} 
