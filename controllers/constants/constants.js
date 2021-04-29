85fa0bd6195a093c1e482b327673396aa1cf2d4b
josemartinrocha <jmmr@holacons.com>
2021-04-29 15:58:36 +0200

    new constants for new project

--- controllers/constants/constants.js
+++ controllers/constants/constants.js
@@ -1,8 +1,8 @@
 module.exports = {
-    policyIdTestNFT: '79d04870cc49ea029f95e7ad19576981620b4665b921c95f79b2a726',
-    testNFTPath: '/home/anon/nft/test/',
+    policyIdTestNFT: '8bca93f7c97ad9b0e779913575c7241507b26c89bb910e03fb24ffc9',
+    testNFTPath: '/home/anon/nft/endTest/',
     adaWithToken: 1500000, // 10000000 = 10 ADA
     publisherName: 'test.com', // adachess.com
-    minAvailableQtyInUtxo: 3000000, // 3000000 = 3 ADA
-    nftPrice: 2000000,
+    minAvailableQtyInUtxo: 2500000, // 3000000 = 3 ADA. This has to be lower that nftPrice
+    nftPrice: 3000000,  // 2000000 = 2 ADA
 };
