57aa03d213ce6bd84fab900f38e9fce78beba94b
josemartinrocha <jmmr@holacons.com>
2021-05-02 08:16:45 +0200

    new test

--- controllers/constants/constants.js
+++ controllers/constants/constants.js
@@ -1,9 +1,9 @@
 module.exports = {
-    policyIdTestNFT: '8bca93f7c97ad9b0e779913575c7241507b26c89bb910e03fb24ffc9',
-    testNFTPath: '/home/anon/nft/endTest/',
+    policyIdTestNFT: '90c9f45c3c73ba4bfaf4d7de0d9f2581c38e3aed06278cc762cb9f87',
+    testNFTPath: '/home/anon/nft/finalTest/',
     adaWithToken: 1500000, // 10000000 = 10 ADA
     publisherName: 'test.com', // adachess.com
-    minAvailableQtyInUtxo: 2500000, // 3000000 = 3 ADA. This has to be lower that nftPrice
-    nftPrice: 3000000,  // 2000000 = 2 ADA
+    minAvailableQtyInUtxo: 32000000, // 3000000 = 3 ADA. This has to be lower that nftPrice
+    nftPrice: 32000000,  // 32000000 = 32 ADA
     recievePaymentsAddr: 'addr1q9sdzr9an4w5uhu96eppc550p6844zfw9t53l53mr9kycma2l79z02ag4ejmtlqtxtpmypttv84f8avgylftx4azyzrql6tu9d',
 };
